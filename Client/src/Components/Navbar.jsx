86acf6d921e4648fc95f72aeb466fb0d90da3955
Shuvamdt <shuvamdt1230@gmail.com>
2025-06-14 00:15:30 +0530

    Normal Updates

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
               onClick={toggleAccount}
             />
           </div>
-          <div className="md:hidden px-2 absolute right-2 z-10">
+          <div className="md:hidden px-5 absolute right-2 z-10">
             <img
               src="menu.png"
               alt="menu icon"
@@ -48,13 +48,18 @@ const Navbar = () => {
         </div>
         {myAccount && (
           <div>
-            <div className="fixed font-1 w-40 top-20 h-40 right-5 z-5 rounded-lg bg-[#FAA307]"></div>
+            <div className="fixed flex flex-col justify-center items-center font-1 w-40 top-20 h- py-2 right-5 z-5 rounded-lg bg-[#FAA307]">
+              <ul className="flex flex-col items-center justify-center">
+                <li className="py-2">My Account</li>
+                <li className="py-2">Sign out</li>
+              </ul>
+            </div>
           </div>
         )}
         {menuOpen && (
           <div>
             <div className="absolute font-1 flex flex-col items-center w-50 top-0 h-full right-0 z-5 bg-[#E85D04]">
-              <div className="md:hidden flex flex-col justify-end px-2 absolute top-5 right-9 z-10">
+              <div className="md:hidden flex flex-col justify-end absolute top-5 right-9 z-10">
                 <img
                   src="close.png"
                   alt="close icon"
